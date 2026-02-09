3ebd447798c900a143b4c0cf2812c88a20ed3898
Summer-Dinner <ejiogudonald1@gmail.com>
2026-02-09 16:19:12 +0100

    health endpoint success

--- app/app.py
+++ app/app.py
@@ -95,12 +95,12 @@ def if_data():
 
 @app.route('/health')
 def health():
-    """
-    ⚠️ BROKEN HEALTH CHECK - Returns 500 error
-    Use this to test health check failures and AWS target group unhealthy alerts
-    """
-    logger.error("Health check failed!")
-    return "Service Unavailable", 503
+    # """
+    # ⚠️ BROKEN HEALTH CHECK - Returns 500 error
+    # Use this to test health check failures and AWS target group unhealthy alerts
+    # """
+    logger.error("Health check passed!")
+    return "<h1>App is Up</h1>", 200
 
 @app.route('/stress-test')
 def stress_test():
